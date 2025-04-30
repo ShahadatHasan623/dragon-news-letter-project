3449b0053a63c12097508c8f66124c468beee920
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-04-30 22:05:38 +0600

    added header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import headerImg from '../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex flex-col items-center justify-center my-12 gap-3'>
             <img src={headerImg} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-semibold text-accent'>{format(new Date(),"EEEE, MMMM dd , yyyy")}</p>
         </div>
     );
 };
