d4633d9b867093320f52f962ce9fa4728aae2f4f
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-02 16:43:19 +0600

    added erropage and deploy firebase

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import Header from "../../components/Header";
+import NewsLatest from "../../components/NewsLatest";
+import NavBar from "../../components/NavBar";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <Header></Header>
+      <div className="w-11/12 mx-auto space-y-4">
+        <NewsLatest></NewsLatest>
+        <NavBar></NavBar>
+      </div>
+      <div className="min-h-screen flex items-center justify-center flex-col ">
+        <div className="text-center space-y-2">
+          <h1 className="text-red-500 text-4xl font-bold">404</h1>
+          <h1 className="text-xl font-semibold">Something is wrong !</h1>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
